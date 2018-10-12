65de305484122106d781a40373b2b6e27f9c75fb
songyouyu <songyy950208@163.com>
2018-10-12 15:50:44 +0800

    项目优化

--- src/main/java/com/imooc/controller/SellerProductController.java
+++ src/main/java/com/imooc/controller/SellerProductController.java
@@ -11,7 +11,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
